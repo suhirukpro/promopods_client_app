de83b6dff59a2cda673799a6e680599359f4b5a4
Sukirtharan Rajadurai <sukirpro@gmail.com>
2023-03-07 19:23:34 +0000

    Modified enpoint

--- src/services/endpoint/index.js
+++ src/services/endpoint/index.js
@@ -16,7 +16,15 @@ export const UPDATE_COHORT = `/Cohort/PutCohort`;
 export const DELETE_COHORT = `Cohort/DeleteCohort`;
 
 //Customers
-export const GET_CUSTOMER = `/Customer/GetAllCustomer`;
+export const GET_CUSTOMERS = `/Customer/GetAllCustomer`;
+export const GET_CUSTOMER = `/Customer/GetCustomer`;
 export const CREATE_CUSTOMER = `/Customer/PostCustomer`;
-export const UPDATE_CUSTOMER = `/Customer/PutCustomer`;
+export const UPDATE_CUSTOMER = `/Customer/updateCustomer`;
 export const DELETE_CUSTOMER = `/Customer/DeleteCustomer`;
+
+//CohortDeliveries
+export const GET_COHORT_DELIVERY = `/CohortDelivery/GetAllCohortDelivery`;
+export const CREATE_COHORT_DELIVERY = `/CohortDelivery/PostCohortDelivery`;
+export const UPDATE_COHORT_DELIVERY = `/CohortDelivery/PutCohortDelivery`;
+export const DELETE_COHORT_DELIVERY = `/CohortDelivery/DeleteCohortDelivery`;
+export const GET_ALL_CORHORT_DELIVERIES_BY_CORHORT = `/CohortDelivery/GetAllCohortDeliveryByCohort`;
